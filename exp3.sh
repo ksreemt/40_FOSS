8b0335d11ba6cc0ff63438e5032219d434487a9e
ksreemt <ksreemt5753@gmail.com>
2017-03-21 15:28:41 +0530

    File Added

---
+++ exp3.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "The current logged in user is :$USER \n"
+echo "Login name: " logname
+echo "Current shell:$SHELL"
+echo "Home Directory:$HOME"
+echo"operating system type:$OSTYPE"
+echo"Current path  setting:$PATH"
+echo"Current working directory:"pwd
+echo "Currently logged number of users:"users|wc-w
+
